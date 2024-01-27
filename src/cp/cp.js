456c9444d39d5b477c03ad89d7ec7c8816fc320f
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-28 02:07:09 +0300

    feat: impl cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,11 @@
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork('src/cp/files/script.js', args, {
+        stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+    });
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', '...']);
