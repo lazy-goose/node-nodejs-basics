ae4a6a2373b317983880399be11e6f779eda67de
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-28 02:08:03 +0300

    feat: impl wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const inWorkerNthFibonacci = async (n) => {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker('./src/wt/worker.js', { workerData: n });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+    });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    const INIT_COMPUTE = 10;
+    const promises = Array.from({ length: cpus().length })
+        .map((_, cpuIndex) => INIT_COMPUTE + cpuIndex)
+        .map(inWorkerNthFibonacci);
+    const settled = await Promise.allSettled(promises);
+    const results = settled.map(({ status, value = null }) => ({
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: value,
+    }));
+
+    console.log(results);
 };
 
 await performCalculations();
