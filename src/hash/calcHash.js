70aac675c03b637b4a3ae5ae1a4b6e64c1696809
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-28 02:07:26 +0300

    feat: impl hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    await pipeline(
+        createReadStream('src/hash/files/fileToCalculateHashFor.txt'),
+        hash
+    );
+    const digestedHash = hash.digest('hex');
+
+    console.log(digestedHash);
 };
 
 await calculateHash();
