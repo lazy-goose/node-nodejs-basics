69ca5751faf4ab9a0708edb666ff8c372d778548
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-28 02:07:55 +0300

    feat: impl streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const read = async () => {
-    // Write your code here 
+    await pipeline(
+        createReadStream('src/streams/files/fileToRead.txt'),
+        process.stdout
+    );
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { pipeline } from 'stream/promises';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseString = (string) => string.split('').reverse().join('');
+    let transformStream;
+    /* Using generator function */
+    transformStream = async function* (source) {
+        source.setEncoding('utf8');
+        for await (const stringChunk of source) {
+            yield await reverseString(stringChunk);
+        }
+    };
+    /* Using Transform Stream */
+    transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, reverseString(chunk.toString()));
+        },
+    });
+    await pipeline(process.stdin, transformStream, process.stdout);
 };
 
 await transform();
