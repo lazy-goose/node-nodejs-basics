05515fc8e8df576c6186cebfec99fde493a8edd0
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-28 02:07:41 +0300

    feat: impl modules

---
+++ src/modules/esm.mjs
@@ -0,0 +1,44 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const random = Math.random();
+
+let unknownObject;
+
+const lazyJsonImport = async (path) => {
+    return (await import(path, { with: { type: 'json' } })).default;
+};
+
+if (random > 0.5) {
+    unknownObject = await lazyJsonImport('./files/a.json');
+} else {
+    unknownObject = await lazyJsonImport('./files/b.json');
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is '${path.sep}'`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export { unknownObject, myServer };
