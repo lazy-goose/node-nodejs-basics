b265aaa196917fc2d8c183227948ff92147ebc80
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-28 02:06:53 +0300

    feat: impl cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,38 @@
 const parseArgs = () => {
-    // Write your code here 
+    const [execPath, filePath, ...cmdArgs] = process.argv;
+
+    const args = {};
+
+    let argName = '';
+    let argVals = [];
+
+    for (let i = 0; i < cmdArgs.length; i++) {
+        if (cmdArgs[i].startsWith('-')) {
+            // Append defined argument
+            if (argName) {
+                args[argName] = argVals;
+            }
+            argName = cmdArgs[i];
+            argVals = [];
+        } else {
+            argVals = [...argVals, cmdArgs[i]];
+        }
+    }
+
+    const argsString = Object.entries(args)
+        .map(([name, values]) => [name.replace(/^--?/, ''), values])
+        .map(([name, values]) => {
+            if (values.length > 1) {
+                return `${name} are [${values.join(', ')}]`;
+            }
+            if (values.length === 1) {
+                return `${name} is ${values[0]}`;
+            }
+            return `${name}`;
+        })
+        .join(', ');
+
+    console.log(argsString);
 };
 
 parseArgs();
