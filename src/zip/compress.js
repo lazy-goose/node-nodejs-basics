ad5e59f5fff075665e818caaefef4b1034f973db
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-28 18:22:43 +0300

    fix(zip): fileToCompress.txt.gz -> archive.gz

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -4,8 +4,9 @@ import { pipeline } from 'stream/promises';
 
 const compress = async () => {
     const src = new URL('files/fileToCompress.txt', import.meta.url);
+    const dst = new URL('files/archive.gz', import.meta.url);
     const readStream = createReadStream(src);
-    const writeStream = createWriteStream(src + '.gz');
+    const writeStream = createWriteStream(dst);
     await pipeline(readStream, createGzip(), writeStream);
 };
 
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -3,9 +3,10 @@ import { createReadStream, createWriteStream } from 'fs';
 import { pipeline } from 'stream/promises';
 
 const decompress = async () => {
-    const src = new URL('files/fileToCompress.txt', import.meta.url);
-    const readStream = createReadStream(src + '.gz');
-    const writeStream = createWriteStream(src);
+    const src = new URL('files/archive.gz', import.meta.url);
+    const dst = new URL('files/fileToCompress.txt', import.meta.url);
+    const readStream = createReadStream(src);
+    const writeStream = createWriteStream(dst);
     await pipeline(readStream, createGunzip(), writeStream);
 };
 
