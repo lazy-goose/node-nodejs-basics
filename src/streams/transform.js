1b224b9a8350f2b7d1a3e753915a50afc5cd3e71
Lazy Goose <shamal.ma.rss@gmail.com>
2024-01-29 17:54:36 +0300

    fix(streams:transform): prettify transform output

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -14,7 +14,8 @@ const transform = async () => {
     /* Using Transform Stream */
     transformStream = new Transform({
         transform(chunk, encoding, callback) {
-            callback(null, reverseString(chunk.toString()));
+            const string = reverseString(chunk.toString().trim())
+            callback(null, string + '\n');
         },
     });
     await pipeline(process.stdin, transformStream, process.stdout);
